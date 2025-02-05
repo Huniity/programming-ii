fb78a421bc323d34ac175897c7f785eda3162ffc
Huniity <adrien.dejonc@gmail.com>
2025-02-05 16:55:26 +0000

    session4 updated challenge

--- Session4/tutorial4.js
+++ Session4/tutorial4.js
@@ -1,13 +1,24 @@
-
+const fs = require('fs');
+    const args = process.argv.slice(2);
+    const inputFile = args[0];
+    const outputFile = args[1];
+    const verbose = args.includes("--verbose");
+    
 
 try {
+    
+    if (!inputFile || !outputFile){
+        throw new Error("The command line should include: <script.js> <input.csv> <output.csv> <flag>");
+    }
 
-    const fs = require('fs');
-    const csvData = fs.readFileSync('Session4_data.csv', 'utf-8');
+    if (!fs.existsSync(inputFile)) {
+        throw new Error(`File "${inputFile}" doesn't exist.`);
+    }
 
+    const csvData = fs.readFileSync(inputFile, 'utf-8');
     const rows = csvData.split('\n');
     const headers = rows[0].split(','); // ["name", "email", "age"]
-
+    
     const data = rows.slice(1).map(row => {
         const values = row.split(',');
         return {
@@ -16,16 +27,23 @@ try {
             age: parseInt(values[2])
         };
     })
-    
-.filter (row => !isNaN(row.age))
-    fs.writeFileSync('session4_data.json', JSON.stringify(data, null, 2));
+        
+    data.forEach((row, index) => {
+        let logs = [];
+        
+        if (!row.name) logs.push("Missing name");
+        if (!row.email) logs.push("Missing email");
+        if (isNaN(row.age)) logs.push("Invalid age");
+        if (verbose && logs.length > 0) {
+            console.log(`Line ${index + 2}: ${logs.join(" | ")} -> ${JSON.stringify(row)}`);
+        }
+    });
+
+    const validData = data.filter(row => row.name && row.email && !isNaN(row.age));
+    fs.writeFileSync(outputFile, JSON.stringify(validData, null, 2));
+    console.log(`File "${outputFile}" has been created and written with success.`)
 }
 
 catch (error) {
     console.error('Error:', error.message);
 }
-
-
-/* if (){
-    throw new Error("Value is not correct")
-} */
