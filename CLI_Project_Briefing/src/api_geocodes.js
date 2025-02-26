63897266dee1baecee1c0a88109c89df80a1a77e
Huniity <adrien.dejonc@gmail.com>
2025-02-26 17:22:19 +0000

    FIX: Fixing Error Handling

--- CLI_Project_Briefing/src/api_geocodes.js
+++ CLI_Project_Briefing/src/api_geocodes.js
@@ -15,13 +15,13 @@ export async function getGeoLocation(city) {
     try {
         const geoRes = await fetch(
             `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`);
-
+        if (!geoRes.ok) throw new Error(`⚠️ Failed to fetch. HTTP Error code: ${res.status} ⚠️`);
             const geoData = await geoRes.json();
-
+        if (!geoData.results?.length) throw new Error('❌ City not found ❌');
 
         return geoData.results[0];
     } catch (error) {
-
+        logger.error(` ❌Geolocation error: ${error.message} ❌`);
         throw error;
     }
 }
