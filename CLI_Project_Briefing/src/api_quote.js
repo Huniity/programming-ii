424456a02e7ef49ea160af04dd1b51c6d99f972a
Huniity <adrien.dejonc@gmail.com>
2025-02-26 16:51:03 +0000

    FEAT: Quote APi Integration

---
+++ CLI_Project_Briefing/src/api_quote.js
@@ -0,0 +1,42 @@
+
+const logger = getLogger("QUOTES", "./winston.logs/quote.logs");
+
+const API_KEY = process.env.API_KEY_QUOTE
+/**
+ * Get Random Quote
+ * @async
+ * @function randomQuote
+ * @returns {Promise<Object>} {quote: string, author: string}
+ * @throws {Error} No Fetch
+ */
+
+export async function randomQuote() {
+  try {
+    const res = await fetch(`https://api.api-ninjas.com/v1/quotes`, {
+      method: "GET",
+      headers: {
+        "X-Api-Key": API_KEY,
+      },
+    });
+
+    
+    const data = await res.json();
+    const filteredQuotes = data.filter((quoteObj) =>
+      quoteObj.quote.length <= 250
+    );
+    if (filteredQuotes.length > 0) {
+      const quoteObject = {
+        quote: filteredQuotes[0].quote,
+        author: filteredQuotes[0].author,
+      };
+      console.log((chalk.whiteBright.bold("\n📜 QUOTE OF THE DAY 📜\n")),);
+      console.log(`"${quoteObject.quote}"\n- ${quoteObject.author}\n`);
+
+
+    } 
+
+
+  } catch (error) {
+    console.log(error)
+  }
+}
